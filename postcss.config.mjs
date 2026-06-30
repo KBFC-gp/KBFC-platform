99dcb8e91fa832053fbcc22781ee1c8dceaf938c
KBFC-gp <b2rsnm9h62@privaterelay.appleid.com>
2026-06-30 13:06:48 -0400

    Delete PostCSS configuration
    Removed PostCSS configuration for Tailwind CSS and Autoprefixer.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -7,12 +7,3 @@ const config = {
 };
 
 export default config;
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
-export default config;
